b68cc60226a8d9c223464f6bd2f36d36eba82663
Farhankaioum <km1472233@gmail.com>
2022-04-12 00:17:52 +0600

    Type added inherit ObjectType

---
+++ CommanderGQL/GraphQL/Platforms/PlatformType.cs
@@ -0,0 +1,16 @@
+﻿using CommanderGQL.Models;
+using HotChocolate.Types;
+
+namespace CommanderGQL.GraphQL.Platforms
+{
+    public class PlatformType : ObjectType<Platform>
+    {
+        protected override void Configure(IObjectTypeDescriptor<Platform> descriptor)
+        {
+            descriptor.Description("Represents any software or service that has a command line interface.");
+
+            descriptor
+                .Field(p => p.LicenseKey).Ignore();
+        }
+    }
+}
