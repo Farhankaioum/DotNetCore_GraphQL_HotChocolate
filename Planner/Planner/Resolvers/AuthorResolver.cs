1dde92d4a27293b3ddd2fa803bdc2c7b70ba9db7
Farhankaioum <km1472233@gmail.com>
2022-04-13 11:33:36 +0600

    Type and resolver added

---
+++ Planner/Planner/Resolvers/AuthorResolver.cs
@@ -0,0 +1,23 @@
+﻿using HotChocolate.Resolvers;
+using Planner.Models;
+using Planner.Repositories;
+
+namespace Planner.Resolvers
+{
+    public class AuthorResolver
+    {
+        private readonly IAuthorRepository _authorRepository;
+
+        public AuthorResolver([Service] IAuthorRepository
+        authorService)
+        {
+            _authorRepository = authorService;
+        }
+
+        public Author GetAuthor(BlogPost blog, IResolverContext ctx)
+        {
+            return _authorRepository.GetAuthors().Where
+            (a => a.Id == blog.AuthorId).FirstOrDefault();
+        }
+    }
+}
---
+++ Planner/Planner/Resolvers/BlogPostResolver.cs
@@ -0,0 +1,21 @@
+﻿using HotChocolate.Resolvers;
+using Planner.Models;
+using Planner.Repositories;
+
+namespace Planner.Resolvers
+{
+    public class BlogPostResolver
+    {
+        private readonly IBlogPostRepository _blogPostRepository;
+
+        public BlogPostResolver([Service] IBlogPostRepository blogPostRepository)
+        {
+            _blogPostRepository = blogPostRepository;
+        }
+
+        public IEnumerable<BlogPost> GetBlogPosts(Author author, IResolverContext ctx)
+        {
+            return _blogPostRepository.GetBlogPosts().Where(b => b.AuthorId == author.Id);
+        }
+    }
+}
