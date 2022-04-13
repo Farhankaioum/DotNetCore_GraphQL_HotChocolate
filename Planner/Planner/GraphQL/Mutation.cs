4e37c03c9b43b0098224bba779b0f5a664a1fa76
Farhankaioum <km1472233@gmail.com>
2022-04-13 11:50:25 +0600

    GraphQL mutation added

---
+++ Planner/Planner/GraphQL/Mutation.cs
@@ -0,0 +1,24 @@
+﻿using HotChocolate.Subscriptions;
+using Planner.Models;
+using Planner.Repositories;
+
+namespace Planner.GraphQL
+{
+    public class Mutation
+    {
+        public async Task<Author> CreateAuthor([Service] AuthorRepository authorRepository, [Service] ITopicEventSender eventSender, int id, string firstName, string lastName)
+        {
+            var data = new Author
+            {
+                Id = id,
+                FirstName = firstName,
+                LastName = lastName
+            };
+
+            var result = await authorRepository.CreateAuthor(data);
+            await eventSender.SendAsync("AuthorCreated", result);
+
+            return result;
+        }
+    }
+}
