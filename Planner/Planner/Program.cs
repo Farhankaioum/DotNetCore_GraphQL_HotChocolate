2784f61917f6d9275ee0aa95c6af3ca55a09a878
Farhankaioum <km1472233@gmail.com>
2022-04-13 11:27:03 +0600

    Blog repository added, registerd in di

--- Planner/Planner/Program.cs
+++ Planner/Planner/Program.cs
@@ -1,10 +1,14 @@
 using Microsoft.EntityFrameworkCore;
 using Planner.Data;
+using Planner.Repositories;
 
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
 builder.Services.AddDbContextFactory<ApplicationDbContext>( options =>
     options.UseInMemoryDatabase("BlogsManagement"));
+
+builder.Services.AddScoped<IAuthorRepository, AuthorRepository>();
+builder.Services.AddScoped<IBlogPostRepository, BlogPostRepository>();
 
 app.Run();
---
+++ Planner/Planner/Repositories/BlogPostRepository.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Planner.Data;
+using Planner.Models;
+
+namespace Planner.Repositories
+{
+    public class BlogPostRepository : IBlogPostRepository
+    {
+        private readonly IDbContextFactory<ApplicationDbContext> _dbContextFactory;
+
+        public BlogPostRepository(IDbContextFactory
+            <ApplicationDbContext> dbContextFactory)
+        {
+            _dbContextFactory = dbContextFactory;
+
+            using (var applicationDbContext = _dbContextFactory.CreateDbContext())
+            {
+                applicationDbContext.Database.EnsureCreated();
+            }
+        }
+        public List<BlogPost> GetBlogPosts()
+        {
+            using (var applicationDbContext = _dbContextFactory.CreateDbContext())
+            {
+                return applicationDbContext.BlogPosts.ToList();
+            }
+        }
+        public BlogPost GetBlogPostById(int id)
+        {
+            using (var applicationDbContext =  _dbContextFactory.CreateDbContext())
+            {
+                return applicationDbContext.BlogPosts.SingleOrDefault(x => x.Id == id);
+            }
+        }
+    }
+}
