d9d217f998ed7df0051dadda8f43960c58c01e7d
Farhankaioum <km1472233@gmail.com>
2022-04-13 11:02:47 +0600

    models added

---
+++ Planner/Planner/Models/BlogPost.cs
@@ -0,0 +1,11 @@
+﻿namespace Planner.Models
+{
+    public class BlogPost
+    {
+        public int Id { get; set; }
+        [GraphQLType(typeof(NonNullType<StringType>))]
+        public string? Title { get; set; }
+        [GraphQLNonNullType]
+        public int AuthorId { get; set; }
+    }
+}
