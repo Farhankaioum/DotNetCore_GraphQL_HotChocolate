9f1796a4e5e06052c0032ac0b32f733c59a26c2e
Farhankaioum <km1472233@gmail.com>
2022-04-13 11:48:04 +0600

    GraphQL query added

--- Planner/Planner/Program.cs
+++ Planner/Planner/Program.cs
@@ -1,14 +1,29 @@
 using Microsoft.EntityFrameworkCore;
 using Planner.Data;
+using Planner.GraphQL;
 using Planner.Repositories;
+using Planner.Types;
 
 var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
 
 builder.Services.AddDbContextFactory<ApplicationDbContext>( options =>
     options.UseInMemoryDatabase("BlogsManagement"));
 
 builder.Services.AddScoped<IAuthorRepository, AuthorRepository>();
 builder.Services.AddScoped<IBlogPostRepository, BlogPostRepository>();
+
+builder.Services
+        .AddGraphQLServer()
+        .AddType<AuthorType>()
+        .AddType<BlogPostType>()
+        .AddQueryType<Query>();
+
+var app = builder.Build();
+
+app.UseRouting()
+    .UseEndpoints(endpoints =>
+    {
+        endpoints.MapGraphQL();
+    });
 
 app.Run();
